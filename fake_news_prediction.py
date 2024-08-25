dc6aa22c0cf49902eec81bfc35a4cef42a3841e3
UncleLimWC <130574436+UncleLimWC@users.noreply.github.com>
2024-08-25 23:31:05 +0800

    Update fake_news_prediction.py

--- fake_news_prediction.py
+++ fake_news_prediction.py
@@ -8,49 +8,47 @@ import string
 lr_loaded = load('logistic_regression_model.joblib')
 tv_loaded = load('tfidfVectorizer.joblib')
 
- # create a function to clean text
+# Create a function to clean text
 def wordopt(text):
-    text = text.lower() # lower case 
-    text = re.sub('\[.*?\]','',text) # remove anything with and within brackets
-    text = re.sub('\\W',' ',text) # removes any character not a letter, digit, or underscore
-    text = re.sub('https?://\S+|www\.\S+','',text) # removes any links starting with https
-    text = re.sub('<.*?>+','', text) # removes anything with and within < >
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text) # removes any string with % in it 
-    text = re.sub('\n','',text) # remove next lines
-    text = re.sub('\w*\d\w*','', text) # removes any string that contains atleast a digit with zero or more characters
+    text = text.lower()  # Lower case 
+    text = re.sub('\[.*?\]', '', text)  # Remove anything with and within brackets
+    text = re.sub('\\W', ' ', text)  # Removes any character not a letter, digit, or underscore
+    text = re.sub('https?://\S+|www\.\S+', '', text)  # Removes any links starting with https
+    text = re.sub('<.*?>+', '', text)  # Removes anything with and within < >
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)  # Removes any string with % in it 
+    text = re.sub('\n', '', text)  # Remove new lines
+    text = re.sub('\w*\d\w*', '', text)  # Removes any string that contains at least a digit with zero or more characters
     return text
-  
 
-# prediction function 
+# Prediction function 
 def news_prediction(news):
-    testing_news = {"text":[news]}
+    testing_news = {"text": [news]}
     new_def_test = pd.DataFrame(testing_news)
     new_def_test['text'] = new_def_test['text'].apply(wordopt)
     new_x_test = new_def_test['text']
-    new_tfidf_test = vectorizer.transform(new_x_test)
-    pred_dt = decision_tree.predict(new_tfidf_test)
+    new_tfidf_test = tv_loaded.transform(new_x_test)
+    pred_dt = lr_loaded.predict(new_tfidf_test)
     
-    if (pred_dt[0] == 0):
-      return "This is Fake News!"
+    if pred_dt[0] == 0:
+        return "This is Fake News!"
     else:
-      return "The News seems to be True!"
-    
- # Streamlit application starts here 
+        return "The News seems to be True!"
+
+# Streamlit application starts here 
 def main():
-     # Title of your web app
-     st.title("Fake News Prediction System")
-     user_text = st.text_area("Enter a sentence to check if it's true or fake:", height - 350)
+    # Title of your web app
+    st.title("Fake News Prediction System")
+    user_text = st.text_area("Enter a sentence to check if it's true or fake:", height=350)
    
-   
-  if st.button("Article Analysis Result"):
-    if user_text:
-    news_pred = news_prediction(user_text)
+    if st.button("Article Analysis Result"):
+        if user_text:
+            news_pred = news_prediction(user_text)
     
-    if (news_pred == "This is Fake News!"):
-      st.error(news_pred, icon="🚨")
-    else:
-      st.success(news_pred)
-      st.balloons()
- 
+            if news_pred == "This is Fake News!":
+                st.error(news_pred, icon="🚨")
+            else:
+                st.success(news_pred)
+                st.balloons()
+
 if __name__ == "__main__":
-  main()
+    main()
